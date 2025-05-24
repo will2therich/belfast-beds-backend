abb0e4b4bb96d32c14129f0ef54f26f4d4f07924
William Rich <william.rich@weblabs.dev>
2025-05-24 16:58:00 +0000

    Customer Resource

---
+++ app/Filament/Resources/Ecom/CustomerResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources\Ecom;
+
+use App\Filament\Resources\Ecom\CustomerResource\Pages;
+use App\Filament\Resources\Ecom\CustomerResource\RelationManagers;
+use App\Models\Ecom\Customer;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+
+    protected static ?string $navigationGroup = 'Ecommerce';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Customer Details')
+                    ->columns(2)
+                    ->schema([
+                        Forms\Components\TextInput::make('full_name')
+                            ->required(),
+                        Forms\Components\TextInput::make('email')
+                            ->required(),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('full_name'),
+                Tables\Columns\TextColumn::make('email'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/Ecom/CustomerResource/Pages/EditCustomer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Ecom\CustomerResource\Pages;
+
+use App\Filament\Resources\Ecom\CustomerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCustomer extends EditRecord
+{
+    protected static string $resource = CustomerResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
